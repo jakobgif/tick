2c2b1cae75b849f92c32c61caeb6bfa8933e45b7
Jakob <jakob.frenzel@hotmail.com>
2025-12-10 16:17:56 +0100

    initial backend rust project

---
+++ backend/src/main.rs
@@ -0,0 +1,22 @@
+use axum::{Json, Router, response::IntoResponse, routing::get};
+use serde_json::json;
+
+//https://docs.rs/axum/latest/axum/#example
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/api", get(hello_world));
+
+    // listen globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server started successfully at 0.0.0.0:3000");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> impl IntoResponse {
+    let json_response = json!({
+        "status": "ok",
+        "message": "Hello, World!"
+    });
+    Json(json_response)
+}
