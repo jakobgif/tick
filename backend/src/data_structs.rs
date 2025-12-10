cef5b9bed29d48013b6550097ae8443634ccf490
Jakob <jakob.frenzel@hotmail.com>
2025-12-10 20:56:56 +0100

    added todo item struct

--- backend/src/data_structs.rs
+++ backend/src/data_structs.rs
@@ -2,6 +2,46 @@
 // 10/12/25
 
 use serde::{Deserialize, Serialize};
+use chrono::{DateTime, Utc};
+
+/// Struct that contains all data a todo item consists of
+/// matches the database fields
+#[derive(Deserialize, Serialize)]
+pub struct TodoItem {
+    /// unique id for the todo item
+    id: u64,
+
+    /// title of the todo
+    title: String,
+
+    /// content of the todo item
+    content: String,
+
+    /// flag to indicate if task done or not done
+    done: bool,
+
+    /// priority levels
+    /// 0 most important
+    /// u16::MAX least important
+    priority: u16,
+
+    /// datetime when item was created
+    /// timestamp created on client side
+    /// serialized as epoch seconds
+    #[serde(with = "chrono::serde::ts_seconds")]
+    creation_date: DateTime<Utc>,
+
+    /// datetime when the task should be finished
+    /// serialized as epoch seconds
+    #[serde(with = "chrono::serde::ts_seconds")]
+    goal_date: DateTime<Utc>,
+
+    /// datetime when the task actually was finished
+    /// timestamp created on client side
+    /// serialized as epoch seconds
+    #[serde(with = "chrono::serde::ts_seconds")]
+    finish_date: DateTime<Utc>
+}
 
 /// Struct to handle whatever search parameters the application offers
 /// To be used with search_todos()
