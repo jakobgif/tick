d87dadc94776b124628970545754b5633ca30cba
Jakob <jakob.frenzel@hotmail.com>
2025-12-22 18:59:58 +0100

    added menu sheet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { useTheme } from "./components/theme-provider";
-import { H2, Muted, P } from "./components/ui/typography";
+import { H1, H2, Muted, P } from "./components/ui/typography";
 import { Badge } from "./components/ui/badge";
 import { Button } from "./components/ui/button";
-import { Moon, Sun } from "lucide-react";
+import { Menu, Moon, Sun } from "lucide-react";
 import { columns, TodoItem } from "./components/columns";
 import { DataTable, QueryParams } from "./components/data-table";
 import { toast } from "sonner";
 import { SortingState } from "@tanstack/react-table";
+import { Sheet, SheetContent, SheetTrigger } from "./components/ui/sheet";
 
 function App() {
   const { theme, setTheme } = useTheme()
@@ -18,7 +19,8 @@ function App() {
   };
 
   const [todos, setTodos] = useState<TodoItem[]>([]);
-  const [sorting, setSorting] = useState<SortingState>([])
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   function mapSortingToQuery(sorting: SortingState): Pick<QueryParams, "sort_by" | "order"> {
     if (!sorting.length) return {}
@@ -63,10 +65,15 @@ function App() {
             <H2 className="mb-2">tick</H2>
             <P>off your personal todos.</P>
           </div>
-          <Button variant="outline" size="icon-sm" onClick={() => toggleTheme()} className="ml-10">
-            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          </Button>
+          <div className="flex flex-row gap-3">
+            <Button variant="outline" size="icon-sm" onClick={() => toggleTheme()}>
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            </Button>
+            <Button variant="outline" size="icon-sm" onClick={() => setMenuOpen(true)}>
+              <Menu/>
+            </Button>
+          </div>
         </div>
         <div className="flex flex-row items-center gap-1">
           <Muted>Maybe start with</Muted>
@@ -85,7 +92,14 @@ function App() {
         setSorting={setSorting}
       />
 
+      {/* add https://ui.shadcn.com/docs/components/pagination# */}
+
       <Button onClick={fetchTodos}>Refresh Todos</Button>
+
+      <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
+        <SheetContent>
+        </SheetContent>   
+      </Sheet>
     </main>
   );
 }
