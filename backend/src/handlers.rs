ebb1d35cb19b825a0c56e4e21e64726646b883b4
Jakob <jakob.frenzel@hotmail.com>
2025-12-22 18:28:16 +0100

    added logging

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -4,6 +4,7 @@
 use std::collections::HashMap;
 
 use axum::{Json, body::Bytes, extract::{Path, Query, State}, response::IntoResponse};
+use log::{debug, error};
 use serde_json::json;
 use sqlx::sqlite::{SqlitePool, SqliteQueryResult};
 use sqlx::Arguments;
@@ -21,6 +22,8 @@ use crate::data_structs::{Order, QueryParams, SortBy, TodoItem};
 /// ```
 // https://docs.rs/axum/latest/axum/extract/struct.Query.html
 pub async fn list_todos(State(connection): State<SqlitePool>, Query(params): Query<QueryParams>) -> impl IntoResponse {
+    debug!("Called handler list_todos() with {:?}", params);
+
     // pagination
     let count = params.count.unwrap_or(25).clamp(1, 100);
     let offset = params.offset.unwrap_or(0).max(0);
@@ -66,14 +69,20 @@ pub async fn list_todos(State(connection): State<SqlitePool>, Query(params): Que
         .await;
 
     match result {
-        Ok(items) => Json(json!({
-            "status": "ok",
-            "items": items
-        })),
-        Err(e) => Json(json!({
-            "status": "error",
-            "message": e.to_string()
-        })),
+        Ok(items) => {
+            debug!("Handler result: Ok");
+            Json(json!({
+                "status": "ok",
+                "items": items
+            }))
+        }
+        Err(e) => {
+            error!("Handler result: {:?}", e);
+            Json(json!({
+                "status": "error",
+                "message": e.to_string()
+            }))
+        }
     }
 }
 
@@ -84,6 +93,8 @@ pub async fn list_todos(State(connection): State<SqlitePool>, Query(params): Que
 /// ```
 // https://docs.rs/axum/latest/axum/extract/struct.Path.html
 pub async fn get_todo(State(connection): State<SqlitePool>, Path(id): Path<i64>) -> impl IntoResponse {
+    debug!("Called handler get_todo() with ID {:?}", id);
+
     // get database row for specific ID
     let result: Result<TodoItem, sqlx::Error> = sqlx::query_as::<_, TodoItem>("
         SELECT  id, title, content, done, priority,
@@ -96,14 +107,20 @@ pub async fn get_todo(State(connection): State<SqlitePool>, Path(id): Path<i64>)
     .await;
 
     match result {
-        Ok(item) => Json(json!({
-            "status": "ok",
-            "item": item
-        })),
-        Err(e) => Json(json!({
-            "status": "error",
-            "message": e.to_string()
-        })),
+        Ok(item) => {
+            debug!("Handler result: Ok");
+            Json(json!({
+                "status": "ok",
+                "item": item
+            }))
+        }
+        Err(e) => {
+            error!("Handler result: {:?}", e);
+            Json(json!({
+                "status": "error",
+                "message": e.to_string()
+            }))
+        }
     }
 }
 
@@ -115,6 +132,8 @@ pub async fn get_todo(State(connection): State<SqlitePool>, Path(id): Path<i64>)
 /// curl -X PUT http://localhost:3000/todos/10 -d '{"content":"","creation_date":0,"done":true,"finish_date":0,"due_date":0,"id":0,"priority":0,"title":""}'
 /// ```
 pub async fn update_todo(State(connection): State<SqlitePool>, Path(id): Path<i64>, body: Bytes) -> impl IntoResponse {
+    debug!("Called handler update_todo() with {:?}", body);
+    
     //try to parse request body
     let parsed: Result<TodoItem, serde_json::Error> = serde_json::from_slice(&body);
 
@@ -122,6 +141,7 @@ pub async fn update_todo(State(connection): State<SqlitePool>, Path(id): Path<i6
     let payload = match parsed {
         Ok(p) => p,
         Err(e) => {
+            error!("Invalid JSON: {}", e);
             return Json(json!({
                 "status": "error",
                 "message": format!("Invalid JSON: {}", e)
@@ -150,18 +170,23 @@ pub async fn update_todo(State(connection): State<SqlitePool>, Path(id): Path<i6
         Ok(res) => {
             //ID does not exits = no row got updated
             if res.rows_affected() == 0 {
+                error!("Todo with ID {} does not exist", id);
                 return Json(json!({
                     "status": "error",
                     "message": format!("Todo with ID {} does not exist", id)
                 }));
             }
 
+            debug!("Handler result: Ok");
             Json(json!({ "status": "ok" }))
         }
-        Err(e) => Json(json!({
-            "status": "error",
-            "message": e.to_string()
-        })),
+        Err(e) => {
+            error!("Handler result: {:?}", e);
+            Json(json!({
+                "status": "error",
+                "message": e.to_string()
+            }))
+        }
     }
 }
 
@@ -171,6 +196,8 @@ pub async fn update_todo(State(connection): State<SqlitePool>, Path(id): Path<i6
 /// curl -X GET http://localhost:3000/todos/autocomplete?q=Hello,%20World!
 /// ```
 pub async fn autocomplete_todos(State(connection): State<SqlitePool>, Query(params): Query<HashMap<String, String>>) -> impl IntoResponse {
+    debug!("Called handler autocomplete_todos() with {:?}", params);
+
     //get the query based on the key "q"
     //return empty json if query string is empty
     let query = match params.get("q") {
@@ -199,14 +226,20 @@ pub async fn autocomplete_todos(State(connection): State<SqlitePool>, Query(para
     .await;
 
     match result {
-        Ok(items) => Json(json!({
-            "status": "ok",
-            "items": items
-        })),
-        Err(e) => Json(json!({
-            "status": "error",
-            "message": e.to_string()
-        })),
+        Ok(items) => {
+            debug!("Handler result: Ok");
+            Json(json!({
+                "status": "ok",
+                "items": items
+            }))
+        }
+        Err(e) => {
+            error!("Handler result: {:?}", e);
+            Json(json!({
+                "status": "error",
+                "message": e.to_string()
+            }))
+        }
     }
 }
 
@@ -216,6 +249,8 @@ pub async fn autocomplete_todos(State(connection): State<SqlitePool>, Query(para
 /// curl -X DELETE http://localhost:3000/todos/42
 /// ```
 pub async fn delete_todo(State(connection): State<SqlitePool>, Path(id): Path<i64>) -> impl IntoResponse {
+    debug!("Called handler delete_todo() with ID {}", id);
+
     //delete row from database
     let result: Result<SqliteQueryResult, sqlx::Error> = sqlx::query("
         DELETE FROM todos WHERE id = ?
@@ -228,17 +263,22 @@ pub async fn delete_todo(State(connection): State<SqlitePool>, Path(id): Path<i6
         Ok(res) => {
             //ID does not exits = no row got updated
             if res.rows_affected() == 0 {
+                error!("Todo with ID {} does not exist", id);
                 return Json(json!({
                     "status": "error",
                     "message": format!("Todo with ID {} does not exist", id)
                 }));
             }
 
+            debug!("Handler result: Ok");
             Json(json!({ "status": "ok" }))
         }
-        Err(e) => Json(json!({
-            "status": "error",
-            "message": e.to_string()
-        })),
+        Err(e) => {
+            error!("Handler result: {:?}", e);
+            Json(json!({
+                "status": "error",
+                "message": e.to_string()
+            }))
+        }
     }
 }
